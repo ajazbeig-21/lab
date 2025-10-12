f879941717be89953edbff73a67f63dae4e4229d
Ajaz Beig <ajazbeig@Ajazs-MacBook-Air.local>
2025-10-12 20:26:41 +0530

    route table association added

--- aws-infra-provision-terraform/main.tf
+++ aws-infra-provision-terraform/main.tf
@@ -12,3 +12,36 @@ resource "aws_subnet" "subnet1"{
     availability_zone = "us-east-1a"
     map_public_ip_on_launch = true
 }
+
+resource "aws_subnet" "subnet2"{
+    vpc_id = aws_vpc.my_vpc.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1b"
+    map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "rtable" {
+  vpc_id = aws_vpc.my_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "rtableassociation" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rtable.id
+}
+
+resource "aws_route_table_association" "rtableassociation2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.rtable.id
+}
