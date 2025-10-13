9e1c8ea24c336abd717bf9b5baa49422d925e158
Ajaz Beig <ajazbeig@Ajazs-MacBook-Air.local>
2025-10-13 07:41:47 +0530

    subnet added

--- aws-infra-provision-terraform/main.tf
+++ aws-infra-provision-terraform/main.tf
@@ -103,7 +103,7 @@ resource "aws_instance" "web" {
     ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (HVM), SSD Volume Type
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.my_security_group.id]
-    
+    subnet_id = aws_subnet.subnet1.id
     tags = {
         Name = "WebServerInstance"
     }
